2a5b1735ecf9c7dc6fca78b01c6aee8e5a10b2b9
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-15 19:58:38 +0530

    added Generator,Hero,Workout components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+      <p>It's time to get</p>
+      <h1>FIT</h1>
+      <p>I hereby acknowledgement that i may become <span>unbelievable fit</span> and accept all risks of becomming the local <span>mass montrosity</span>,affilicted with severe body dismorphia,unable to fit through doors</p>
+    </div>
+  )
+}
