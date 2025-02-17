9b883e124a100f2d5d0c97817a990599de73b46f
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-17 20:06:28 +0530

    added section wrapper components

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+import { WORKOUTS } from '../utill/swoldier'
 
 export default function Generator() {
+
+  function Header(props){
+
+    const {index,title,description} = props
+
+    return(
+      <div className='flex flex-col gap-4'>
+        <div className='flex items-center justify-center gap-2'>
+          <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+          <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
+        </div>
+        <p className='text-sm sm:text-base max-auto'>{description}</p>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      Generator
-    </div>
+    <SectionWrapper header={"generate your workout"} title=
+    {['It\'s', 'Huge' , 'o\'clock']}>
+      <Header index={'1'} title={'Choose your workout'} description={'Select the workout you want to do'}/>
+      
+      <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+        {Object.keys(WORKOUTS).map((type,typeIndex) =>{
+          return <button className='bg-slate-950 border border-blue-400 py-3 rounded-lg duration-200 hover:border-blue-600' key={typeIndex}>
+                  <p className='capitalize'>{type.replaceAll('_'," ")}</p>
+                </button>
+        })}
+
+      </div>
+
+    </SectionWrapper>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
-      <p>It's time to get</p>
-      <h1>FIT</h1>
-      <p>I hereby acknowledgement that i may become <span>unbelievable fit</span> and accept all risks of becomming the local <span>mass montrosity</span>,affilicted with severe body dismorphia,unable to fit through doors</p>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center max-w-[800px] w-full mx-auto p-4'>
+      <div className='flex flex-col gap-4'>
+
+        <p>It's time to get</p>
+        <h1 className='uppercase font-bold text-5xl sm:text-5xl md:text-6xl lg:text-7xl'>sha<span className='text-blue-400 font-medium'>pe</span></h1>
+      </div>
+      <p className='text-sm md:text-base font-light'>I hereby acknowledgement that i may become <span className='text-blue-400 font-medium'>unbelievable fit</span> and accept all risks of becomming the local <span>mass montrosity</span>,affilicted with severe body dismorphia,unable to fit through doors</p>
+      <button className='px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueshadow duration-200'>
+        <p>Accept & Begin</p>
+        </button>   
     </div>
   )
 }
